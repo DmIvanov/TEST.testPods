46681bff7187b46d358cca68d6507c16b17d0d5f
DmIvanov <topolog@icloud.com>
2015-10-29 15:02:22 +0300

    initial

---
+++ testPods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testPods
+//
+//  Created by DmIvanov on 29.10.15.
+//  Copyright © 2015 DI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
